545cde3cdb81fd0d13e49d0249bc8a37c377b008
Blaire Baker <bakerblaire@gmail.com>
2021-05-06 20:35:42 -0600

    add close function to dialog

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dialog } from '@material-ui/core';
 import Confetti from 'react-dom-confetti'
 
 function Banner(){
+    const [open, setOpen] = useState(true)
+
+    const handleClose = () => {
+        setOpen(false)
+      }
+
     return(
-        <Dialog open="true">
-            
+        <Dialog open={open} onClick={handleClose}> 
             <h2 className="banner"><Confetti active="true"/>Thank you for participating in The Shoppies!</h2>
         </Dialog>
     )
